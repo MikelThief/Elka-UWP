449e9a30434fce169a0b9518700c90a2aa64e030
MikelThief <m.bator2@gmail.com>
2018-12-23 22:22:08 +0100

    Upgraded solution to use Microsoft.NETCore.UniversalWindowsPlatform 6.2.3 because of https://github.com/Microsoft/dotnet/issues/925

--- ElkaUWP.Data/VoltaContext.cs
+++ ElkaUWP.Data/VoltaContext.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Microsoft.EntityFrameworkCore;
 
 namespace ElkaUWP.Data
 {
-    public class VoltaContext : DbContext
+    public class VoltaContext
     {
 
     }
