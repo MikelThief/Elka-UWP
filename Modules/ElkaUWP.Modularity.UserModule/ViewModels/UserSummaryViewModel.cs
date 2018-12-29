8d0211c0740b5c82cf8207508a5e95cf4181d525
MikelThief <m.bator2@gmail.com>
2018-12-29 15:58:00 +0100

    Resources resolution fixed for LoginModule

---
+++ Modules/ElkaUWP.Modularity.UserModule/ViewModels/UserSummaryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace ElkaUWP.Modularity.UserModule.ViewModels
+{
+    public class UserSummaryViewModel : BindableBase
+    {
+        private Image _userImage;
+
+        public Image UserImage { get => _userImage; private set => SetProperty(storage: ref _userImage, value: value); }
+    }
+}
