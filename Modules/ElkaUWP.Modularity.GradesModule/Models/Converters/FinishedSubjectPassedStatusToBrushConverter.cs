bf1ed855c0525a6d6203db22b7ddeca2449aa78b
Michał Bator <m.bator2@gmail.com>
2019-03-11 13:35:52 +0100

    Improved grades layout

---
+++ Modules/ElkaUWP.Modularity.GradesModule/Models/Converters/FinishedSubjectPassedStatusToBrushConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace ElkaUWP.Modularity.GradesModule.Models.Converters
+{
+    class FinishedSubjectPassedStatusToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool castedValue)
+            {
+                SolidColorBrush returnedBrush;
+
+                if (castedValue)
+                    return new SolidColorBrush(Colors.LightGreen);
+                else
+                    return new SolidColorBrush(Colors.Crimson);
+
+            }
+            else throw new ArgumentException(message: nameof(FinishedSubjectPassedStatusToBrushConverter) +
+                                                      "doesn't support type" + targetType.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
