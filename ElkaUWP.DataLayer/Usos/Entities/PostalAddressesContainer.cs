c683c41985e98d2abaf5ee7add66c67bd5b45a8d
Dreidree <a.sobota96@gmail.com>
2019-03-30 16:08:55 +0100

    v16

--- ElkaUWP.DataLayer/Usos/Entities/PostalAddressesContainer.cs
+++ ElkaUWP.DataLayer/Usos/Entities/PostalAddressesContainer.cs
@@ -14,15 +14,5 @@ namespace ElkaUWP.DataLayer.Usos.Entities
 
     }
 
-    public class PostalAddresses
-    {
-        [JsonProperty("type")]
-        public string Type { get; set; }
-
-        [JsonProperty("address")]
-        public string Address { get; set; }
-
-        [JsonProperty("type_name")]
-        public LangDict TypeName { get; set; }
-    }
+   
 }
