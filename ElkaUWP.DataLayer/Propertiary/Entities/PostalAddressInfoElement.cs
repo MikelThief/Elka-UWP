a32707c6952ea564f4626e123f089f8f3f7a9f7f
Agnieszka Sobota <a.sobota96@gmail.com>
2019-04-03 00:33:59 +0200

    v18

---
+++ ElkaUWP.DataLayer/Propertiary/Entities/PostalAddressInfoElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElkaUWP.DataLayer.Propertiary.Entities
+{
+    public class PostalAddressInfoElement
+    {
+        public string type { get; set; }
+        public string address { get; set; }
+        public string type_name { get; set; }
+    }
+}
