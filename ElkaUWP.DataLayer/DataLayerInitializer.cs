f5e2787f2f81a615fdd2ce90a5759bf023299eaf
MikelThief <m.bator2@gmail.com>
2019-04-03 23:54:04 +0200

    Fixes to architecture

--- ElkaUWP.DataLayer/DataLayerInitializer.cs
+++ ElkaUWP.DataLayer/DataLayerInitializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ElkaUWP.DataLayer.Propertiary;
 using ElkaUWP.DataLayer.Usos.Requests;
 using ElkaUWP.DataLayer.Usos.Services;
 using Prism.Ioc;
