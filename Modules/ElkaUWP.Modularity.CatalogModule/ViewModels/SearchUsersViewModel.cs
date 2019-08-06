59813df09e37bb67482b4cd89584ba1f777c1c9d
MikelThief <m.bator2@gmail.com>
2019-08-06 21:26:40 +0200

    Created proprietary service for searching users

--- Modules/ElkaUWP.Modularity.CatalogModule/ViewModels/SearchUsersViewModel.cs
+++ Modules/ElkaUWP.Modularity.CatalogModule/ViewModels/SearchUsersViewModel.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ElkaUWP.DataLayer.Usos.Requests;
+using ElkaUWP.DataLayer.Usos.Services;
 using Prism.Mvvm;
 
 namespace ElkaUWP.Modularity.CatalogModule.ViewModels
 {
     public class SearchUsersViewModel : BindableBase
     {
+        private readonly UsersService _usersSerice;
 
+        public ObservableCollection<string> SuggestedItems;
+        public SearchUsersViewModel(UsersService usersSerice)
+        {
+            _usersSerice = usersSerice;
+        }
+
+        public async Task SearchUsersAsync(string query, UserCategory category)
+        {
+            var result = await _usersSerice.Search2Async(query: query, userCategory: UserCategory.CurrentStaff);
+
+            if (result.IsSuccess && result.Value.HasValue)
+            {
+
+            }
+
+
+        }
     }
 }
